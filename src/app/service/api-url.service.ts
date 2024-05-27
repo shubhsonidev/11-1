04074c9859881a1a28cc0ee9233fcca49579a361
Shubh soni <shubhsoni.dev@gmail.com>
2024-05-27 15:50:38 +0530

    intital commit

--- src/app/service/api-url.service.ts
+++ src/app/service/api-url.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiUrlService {
-  public url: any = "https://script.google.com/macros/s/AKfycbyL7qnL9xuhaz5jyLeEuHq69ItOlDKTDpDxBq7R-vFwAeK2R-YfqWt38gjOKfhQ1jk/exec?";
-  constructor() {
-  }
+  public url: any =
+    'https://script.google.com/macros/s/AKfycbx-u0SyRyAHaKj1RGT2no0iGn0z6QhkJCCxdoqA03bf5TBEzNlxR9cqdTDJXJkwuwH9Xg/exec?';
+  constructor() {}
 }
